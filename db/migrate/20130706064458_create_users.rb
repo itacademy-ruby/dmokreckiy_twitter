f620633507bc66fa86e0c818648340cff15d24d2
Alexander Randa <randa.alex@active.by>
2013-07-06 10:05:18 +0300

    db adapter changed to mysql2

--- db/migrate/20130706064458_create_users.rb
+++ db/migrate/20130706064458_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :login
       t.string :email
+      t.string :login
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130629070448) do
+ActiveRecord::Schema.define(:version => 20130706064605) do
 
   create_table "microposts", :force => true do |t|
     t.integer  "user_id"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(:version => 20130629070448) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "login"
     t.string   "email"
+    t.string   "login"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
